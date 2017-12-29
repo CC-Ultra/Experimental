b8b100b5ae030259e79f628637bd9439e98083cd
snayper <cc-ultra@bk.ru>
2017-12-29 20:33:42 +0300

    Отправка длинного SMS

---
+++ app/src/main/java/com/ultra/experimental/Utils/O.java
@@ -0,0 +1,19 @@
+package com.ultra.experimental.Utils;
+
+/**
+ * Created by CC-Ultra on 20.11.2017.
+ */
+
+public class O
+	{
+	public static final String TAG="c123";
+	public static final String ACTION="com.ultra.experimental.MainActivity";
+
+	public static class mapKeys
+		{
+		public static class extra
+			{
+			public static final String BROADCAST_STATIC_INT="Broadcast static int";
+			}
+		}
+	}
